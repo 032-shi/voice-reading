e92b19ebf6fd46ec10ea180eaf053318d402c14c
032-shi <0324omitsu@gmail.com>
2021-08-05 13:14:20 +0900

    [fix]動作確認後、コメント追記

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # coding: UTF-8
+# 上の1行で日本語対応させる
 import pyttsx3
-engine = pyttsx3.init()
+engine = pyttsx3.init() #ライブラリの初期化
 engine.save_to_file("出力音声をここに入力する","test.mp3")
-engine.runAndWait()
+engine.runAndWait() #実行処理
